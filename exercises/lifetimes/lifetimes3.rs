c40b6295a9b43a763e9af38db19800b10d9b9eb3
Loïc Séguin-Charbonneau <lsc@loicseguin.com>
2023-08-04 21:51:00 -0400

    Ifs, lifetimes and tests

--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -5,14 +5,13 @@
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
 }
 
 fn main() {
+    let bob = String::from("Bob");
     let name = String::from("Jill Smith");
     let title = String::from("Fish Flying");
     let book = Book { author: &name, title: &title };
